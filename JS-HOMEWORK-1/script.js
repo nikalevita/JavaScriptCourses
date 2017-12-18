14992556055adc635ed981aab29f83dbd2fcde18
a.Kuz <andrew.kuz@94gmail.com>
2017-12-18 17:02:20 +0200

    Alx: homework-1

--- JS-HOMEWORK-1/script.js
+++ JS-HOMEWORK-1/script.js
@@ -2,21 +2,24 @@
 /* qSort */
 function qSort(mainArr){
 
+	/* swap */
 	function swap(arr, indexA, indexB) {
 		var temp = arr[indexA];
 		arr[indexA] = arr[indexB];
 		arr[indexB] = temp;
 	}
-		
+	
+	/* partition */
 	function partition(mainArr, pivot, leftEl, rightEl){
+		
 		var newIndex = leftEl,
 			pivotVal = mainArr[pivot];
 
 		swap(mainArr, pivot, rightEl);
 
-		for(var i = 0; i < rightEl - leftEl; i++){
-			if(mainArr[i] <= pivotVal){
-				swap(mainArr, newIndex, i);
+		for(var i = leftEl; i < rightEl; i++){
+			if(mainArr[i] < pivotVal){
+				swap(mainArr, i, newIndex);
 				newIndex++;
 			}
 		}
@@ -26,29 +29,20 @@ function qSort(mainArr){
 		return newIndex;
 	}
 
-	var firstStep = partition(mainArr, parseInt((mainArr.length)-1 / 2), 0, mainArr.length - 1);
-
-	/* ---------- END OK ----------- */
-
-	/* NOT OK */
-	function sort(mainArr, leftEl, rightEl) {
-
-		/*var firstStep = partition(mainArr, parseInt((leftEl + rightEl) / 2), leftEl, rightEl);
-
-		if (leftEl < firstStep - 1){
-			sort(mainArr, leftEl, firstStep-1);
+	/* sort */
+	function sort(mainArr, leftEl, rightEl) {  
+		if(leftEl < rightEl){
+			var firstStep = partition(mainArr, parseInt((leftEl + rightEl) / 2), leftEl, rightEl);
+			sort(mainArr, leftEl, firstStep - 1);
+			sort(mainArr, firstStep + 1, rightEl);
 		}
-		if (rightEl > firstStep){	
-			sort(mainArr, firstStep, rightEl);
-		}
-	*/
 	}
+	
 	sort(mainArr, 0, mainArr.length - 1);
-
 
 	return mainArr;
 }
 
-var mainArr = [5,4,98,11,6,0,20,50,250,6,5,0,100,2];
+var mainArr = [5,4,98,11,6,0,20,50,250,6,5,0,100,2,0];
 console.log(mainArr);
 console.log(qSort(mainArr));
